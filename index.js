516a8ffbc63165640750e23445dcbf6999907aba
Nataliia Matsko <mask.dancing11@gmail.com>
2023-04-21 03:09:53 +0300

    add

---
+++ index.js
@@ -0,0 +1,7 @@
+import "./js/renderMovies.js";
+import "./js/searchMovies.js";
+import "./js/movieModalWindow.js";
+import "./js/spinner.js";
+import "./js/pagination.js";
+import "./js/modal.js";
+
